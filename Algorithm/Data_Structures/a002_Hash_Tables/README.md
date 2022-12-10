32f5aec89122da058e99377ede1dcd8135bea2c4
YoungHa Kim <ytok1108@aol.com>
2022-12-10 22:27:34 +0900

    HashTable

---
+++ Algorithm/Data_Structures/a002_Hash_Tables/README.md
@@ -0,0 +1,29 @@
+# Data Structures - Hash Tables
+
+https://youtu.be/VJxm6_9VaYQ
+
+<br>
+
+# Result:
+```
+$ python3 main.py
+
+INITIAL HASH TABLE
+{1: 'red', 2: 'blue', 3: 'green'}
+----------------
+
+INSERT ELEMENT INTO HASH TABLE
+{1: 'red', 2: 'blue', 3: 'green', 4: 'orange'}
+----------------
+
+GET ELEMENT FROM HASH TABLE
+blue
+----------------
+
+UPDATE ELEMENT IN HASH TABLE
+{1: 'yellow', 2: 'blue', 3: 'green', 4: 'orange'}
+----------------
+
+DELETE ELEMENT FROM HASH TABLE
+{2: 'blue', 3: 'green', 4: 'orange'}
+```
---
+++ Algorithm/Data_Structures/a002_Hash_Tables/main.py
@@ -0,0 +1,33 @@
+hashtable = {
+        1: "red",
+        2: "blue",
+        3: "green"
+        }
+print("\nINITIAL HASH TABLE")
+print(hashtable)
+
+# INSERT ELEMENT INTO HASH TABLE
+print("----------------")
+print("\nINSERT ELEMENT INTO HASH TABLE")
+hashtable[4] = "orange"
+print(hashtable)
+
+
+# GET ELEMENT FROM HASH TABLE
+print("----------------")
+print("\nGET ELEMENT FROM HASH TABLE")
+element = hashtable.get(2, "purple")
+print(element)
+
+# UPDATE ELEMENT IN HASH TABLE
+print("----------------")
+print("\nUPDATE ELEMENT IN HASH TABLE")
+hashtable[1] = "yellow"
+print(hashtable)
+
+# DELETE ELEMENT FROM HASH TABLE
+print("----------------")
+print("\nDELETE ELEMENT FROM HASH TABLE")
+del hashtable[1]
+print(hashtable)
+
